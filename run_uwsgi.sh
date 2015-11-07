55f14bc87e6bf39b5d2c62d4efbd3fdb61e58cb1
vsoch <vsochat@stanford.edu>
2015-11-07 09:06:06 -0800

    updating init scripts to import database

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-python manage.py syncdb
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+# Must be done twice for dependencies
+python manage.py bash scripts/import_database.sh
+python manage.py bash scripts/import_database.sh
 uwsgi uwsgi.ini
